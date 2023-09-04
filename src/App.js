1825c28eb56b49b412cf551d97d25406219bd455
mateunix <mateusagre@yandex.com>
2023-09-04 12:41:10 -0300

    Adding login page and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+
+Pontificia Universidade Católica do Paraná
+Nome: Mateus VInicius Franco Agre
+Disciplina: Tecnologias Para Desenvolvimento Web
+Professor: Felipe Elias
+
+*/
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
+  //React states
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  //Condição de Login
+   const handleLogin = () => {
+    if (email === 'eduardo.lino@pucpr.br' && senha === '123456') {
+      setMensagem('Acessado com sucesso!');
+    } else {
+      setMensagem('Usuário ou senha incorretos!');
+    }
+  };
+
+  //Layout da página (label sem nome (email ou senha) para corresponder a imagem do app do exercício)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      <div>
+        <label></label>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label></label>
+        <input
+          type="password"
+          placeholder="senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Acessar</button>
+      <p>{mensagem}</p>
     </div>
   );
 }
